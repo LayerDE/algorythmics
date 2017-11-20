4e63293ba84bbc64e6c150f89fafc9b28fd65a05
LayerDE <the_gamer@gmx.net>
2017-11-20 01:36:11 +0100

    blatt 4.1

---
+++ uebungsblatt_04/geo_names_analyzer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def read_info_from_file(path)
+    tmp=[]
+    tmp2
+    with open(path, 'r') as big_list
+        line = big_list.readline()
+        while line:
+            tmp2=line.split('\t')
+            if(tmp2[14]>0)
+                tmp.append(tmp2)
+            line = big_list.readline()
+    return tmp
+
+def compute_names_by_sorting(path)
+    tmp=read_info_from_file(path)
+    name_listing=[]
+    for x in tmp:
+
+def compute_names_by_map(path)
+    
